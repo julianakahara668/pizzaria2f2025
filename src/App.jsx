e91bef60c4bcfade6ba12e6d5610088ec65d5884
julianakahara668 <rm95317@estudante.fieb.edu.br>
2025-03-28 09:57:41 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App(){
 
   //var mome = "Julia";
 
-  return (
-    <div>
-        <h3>Pizzaria 2F</h3>
-        <input
+  const Formulario = () => {
+    return (
+      <div>
+          <input
         className="nome"
         name = "nome"
         onChange={(e)=>{setNome=(e.target.value)}}
@@ -20,6 +20,16 @@ function App(){
         onClick={()=> {alert(nome)}}>
         CLIQUE AQUI 
         </button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+        <h3>Pizzaria 2F</h3>
+        <Formulario/>
+        <Formulario/>
+        <Formulario/>
     </div>
   )
 }
