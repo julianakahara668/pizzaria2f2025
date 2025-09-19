cf173c0a87ec358a10a96679eb277669cd9b7cac
julianakahaea668 <rm95317@estudante.fieb.edu.br>
2025-09-19 08:18:41 -0300

    lista

--- src/page/produtos/index.jsx
+++ src/page/produtos/index.jsx
@@ -1,4 +1,13 @@
 const Produtos =() => {
+//Objeto com a lista de pizzas
+const pizzas =[
+    'Pizza de Muçarela',
+    'pizza de Calabresa',
+    'Pizza Baiana', 'Pizza Quatro Queijo '
+]
+// Interação da lista de pizzas 
+
+const listaPizzas = pizzas.map(pizzas=><li>{pizza}</li>)
     return (
         <div>
             <ul>
